4fc29af92cad91c4884e985a95d324bb66e86bfa
lawther1206 <97736038+lawther1206@users.noreply.github.com>
2023-12-02 09:31:54 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# ly-table-next
+# ly-table-next 基于 element 简单封装的 table
 
-npm i ly-table-next
+# 1.npm 安装
 
+    <!-- 需要安装element plus -->
+    npm i ly-table-next
 
+# 2. main.js/ts 引入
+
+    import LyTableLast from "ly-table-last";
+    app.use(LyTableLast)
+
+# 3. 组件内使用
+
+       <LyTableLast />
+
+# 4.示例图片
+  ![示例图片](src/assets/table.png)
+
+# npm发布方案
+   详情见(https://blog.csdn.net/weixin_46194350/article/details/134685465)
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,71 @@
 <template>
   <div>
-    <LyTableLast />
+    <LyTableLast
+      :tableData="state.tableData"
+      :tableHead="state.tableHead"
+      :config="{}"
+    >
+      <!-- 数据展示插槽 -->
+      <template #otherProp="{ row }">
+        {{ row }}
+      </template>
+
+      <!-- 扩展列插槽 -->
+      <template #expand="{ row }">
+        <el-button type="primary" @click="edit(row)"> 编辑</el-button>
+        <el-button @click="edit(row)"> 删除</el-button>
+      </template>
+    </LyTableLast>
   </div>
 </template>
 
 <script setup>
+import { reactive } from "vue";
 
+const state = reactive({
+  tableData: [
+    {
+      age: 1,
+      sex: "男",
+    },
+    {
+      age: 2,
+      sex: "男",
+    },
+    {
+      age: 3,
+      sex: "女",
+    },
+  ],
+  tableHead: [
+    {
+      label: "性别",
+      prop: "sex",
+      // width: 200,
+      // fixed: 'left',
+      // align: 'left',
+      // headAlign: 'center',
+    },
+    {
+      label: "年龄",
+      prop: "age",
+      // minWidth: 500,
+      // align: 'center',
+      // headAlign: 'center',
+    },
+    {
+      label: "其他",
+      propName: "otherProp",
+      // minWidth: 700,
+      // align: 'right',
+      // headAlign: 'center',
+    },
+  ],
+});
 </script>
 
-<style scoped></style>
-
+<style scoped>
+:deep(.el-table__expand-column .cell) {
+  display: none;
+}
+</style>
